5b50a48c1e9eb68c68e87869ba30efdc62100929
Joël Gähwiler <joel.gaehwiler@gmail.com>
2015-01-31 11:42:44 +0100

    custom mosquitto config

--- mosq.sh
+++ mosq.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-echo "===== LAUNCHING MOSQUITTO ====="
-
-mosquitto -c /usr/local/etc/mosquitto/mosquitto.conf -p 5000 &
+mosquitto -c mosq.conf -p 5000 &
 sleep 2
-
-echo "===== START TESTS ====="
 
 export PORT=5000
 ./node_modules/.bin/mocha --recursive --reporter list test/acceptance/**/*.js
-
-echo "===== STOPPING MOSQUITTO ====="
 
 killall mosquitto
